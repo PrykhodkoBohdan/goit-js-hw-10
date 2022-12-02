65283da9d9173b4130a27c46e6a239e5c91a6300
pickfix <Prikhodko.B.T@gmail.com>
2022-12-02 18:38:37 +0300

    Create refs.js

---
+++ src/refs.js
@@ -0,0 +1,6 @@
+ const refs = {
+    input: document.querySelector('input'),
+    list: document.querySelector('.country-list'),
+    info: document.querySelector('.country-info'),
+  };
+ export const {input,list,info} = refs;
